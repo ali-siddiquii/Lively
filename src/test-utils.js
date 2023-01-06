6c5d8f40c1766bbb25319edf2fb35b9bd3a971fe
siddiqiiali09 <108311273+siddiqiiali09@users.noreply.github.com>
2023-01-06 11:52:03 -0800

    Create test-utils.js

---
+++ src/test-utils.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '/src/styles/theme.jsx';
+
+const AllProviders = ({ children }) => (
+  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+);
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllProviders, ...options });
+
+export { customRender as render };
