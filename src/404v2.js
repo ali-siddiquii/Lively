bc73057a971e8e2c5c96cdc2736d489ca15fac96
siddiqiiali09 <108311273+siddiqiiali09@users.noreply.github.com>
2023-01-06 11:39:49 -0800

    Create 404v2.js

---
+++ src/404v2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ErrorPage2 from './components/errorV2';
+
+function error() {
+  return (
+    <>
+      <ErrorPage2 />
+    </>
+  );
+}
+
+export default error;
